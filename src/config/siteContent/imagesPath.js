dea7d8166cf336864ff80b340eb77945b07ae3b4
CrisSoven <20213tn044@utez.edu.mx>
2025-03-03 17:41:12 -0600

    [MODIFY] change route images

--- src/config/siteContent/imagesPath.js
+++ src/config/siteContent/imagesPath.js
@@ -1,78 +1,78 @@
 const imagesPath = {
   // Logos
-  logo: "/logo/logo-1.svg",
-  whiteLogo: "/logo/logo-2.svg",
+  logo: "../../../logo/logo-1.svg",
+  whiteLogo: "../../../logo/logo-2.svg",
   // Home page background
-  heroBackground: "/images/home/herosection/vrv_system.jpg",
+  heroBackground: "../../../images/home/herosection/vrv_system.jpg",
   // Home carousel (rotation images)
   summer1: {
-    src: "/images/home/carousel/summer_1.jpg",
+    src: "../../../images/home/carousel/summer_1.jpg",
     alt: "Summer Image 1",
   },
   summer2: {
-    src: "/images/home/carousel/summer_2.jpg",
+    src: "../../../images/home/carousel/summer_2.jpg",
     alt: "Summer Image 2",
   },
   winter1: {
-    src: "/images/home/carousel/winter_1.jpg",
+    src: "../../../images/home/carousel/winter_1.jpg",
     alt: "Winter Image 1",
   },
   winter2: {
-    src: "/images/home/carousel/winter_2.jpg",
+    src: "../../../images/home/carousel/winter_2.jpg",
     alt: "Winter Image 2",
   },
   winter3: {
-    src: "/images/home/carousel/winter_3.jpg",
+    src: "../../../images/home/carousel/winter_3.jpg",
     alt: "Winter Image 3",
   },
   // Services images
   services: [
-    "/images/services/hvac_installation.jpg",
-    "/images/services/repair_and_maintenance.jpg",
-    "/images/services/maintenance_plans.jpg",
+    "../../../images/services/hvac_installation.jpg",
+    "../../../images/services/repair_and_maintenance.jpg",
+    "../../../images/services/maintenance_plans.jpg",
   ],
   // HVAC Installation
   hvacInstallation: [
-    "/images/services/hvac/hvac_installation_1.webp",
-    "/images/services/hvac/hvac_installation_2.webp",
-    "/images/services/hvac/hvac_installation_3.webp",
+    "../../../images/services/hvac/hvac_installation_1.webp",
+    "../../../images/services/hvac/hvac_installation_2.webp",
+    "../../../images/services/hvac/hvac_installation_3.webp",
   ],
   // Equipment images
   equipments: [
     {
-      src: "/images/equipment/equipments/condenser.jpg",
+      src: "../../../images/equipment/equipments/condenser.jpg",
       alt: "Condenser",
     },
     {
-      src: "/images/equipment/equipments/forced_air_units.jpg",
+      src: "../../../images/equipment/equipments/forced_air_units.jpg",
       alt: "Forced Air Units",
     },
     {
-      src: "/images/equipment/equipments/heat_pump_forced_air.jpg",
+      src: "../../../images/equipment/equipments/heat_pump_forced_air.jpg",
       alt: "Heat Pump Forced",
     },
     {
-      src: "/images/equipment/equipments/heat_pump_mini_splits.jpg",
+      src: "../../../images/equipment/equipments/heat_pump_mini_splits.jpg",
       alt: "Heat Pump Mini Split",
     },
     {
-      src: "/images/equipment/equipments/heat_pump.jpg",
+      src: "../../../images/equipment/equipments/heat_pump.jpg",
       alt: "Heat Pump",
     },
     {
-      src: "/images/equipment/equipments/ptac_unit.jpg",
+      src: "../../../images/equipment/equipments/ptac_unit.jpg",
       alt: "PTAC Unit",
     },
     {
-      src: "/images/equipment/equipments/rtu_rooftop_packaged_units.jpg",
+      src: "../../../images/equipment/equipments/rtu_rooftop_packaged_units.jpg",
       alt: "RTU Rooftop Packaged Units",
     },
     {
-      src: "/images/equipment/equipments/split_unit_wall_mount_mini_splits.jpg",
+      src: "../../../images/equipment/equipments/split_unit_wall_mount_mini_splits.jpg",
       alt: "Split Unit Wall Mount Mini Splits",
     },
     {
-      src: "/images/equipment/equipments/vrv_vrf_systems.png",
+      src: "../../../images/equipment/equipments/vrv_vrf_systems.png",
       alt: "VRV VRF Systems",
     },
   ],
@@ -80,47 +80,47 @@ const imagesPath = {
   // Brands we service
   brands: [
     {
-      src: "/images/equipment/brands/bryant.png",
+      src: "../../../images/equipment/brands/bryant.png",
       alt: "Bryant Logo",
     },
     {
-      src: "/images/equipment/brands/carrier.png",
+      src: "../../../images/equipment/brands/carrier.png",
       alt: "Carrier Logo",
     },
     {
-      src: "/images/equipment/brands/comfort_aire.png",
+      src: "../../../images/equipment/brands/comfort_aire.png",
       alt: "Comfort Aire Logo",
     },
     {
-      src: "/images/equipment/brands/concord.jpg",
+      src: "../../../images/equipment/brands/concord.jpg",
       alt: "Concord Logo",
     },
     {
-      src: "/images/equipment/brands/daikin.png",
+      src: "../../../images/equipment/brands/daikin.png",
       alt: "Daikin Logo",
     },
     {
-      src: "/images/equipment/brands/fujitsu.png",
+      src: "../../../images/equipment/brands/fujitsu.png",
       alt: "Fujitsu Logo",
     },
     {
-      src: "/images/equipment/brands/goodman.png",
+      src: "../../../images/equipment/brands/goodman.png",
       alt: "Goodman Logo",
     },
     {
-      src: "/images/equipment/brands/lennox.jpg",
+      src: "../../../images/equipment/brands/lennox.jpg",
       alt: "Lennox Logo",
     },
     {
-      src: "/images/equipment/brands/mitsubishi.jpg",
+      src: "../../../images/equipment/brands/mitsubishi.jpg",
       alt: "Mitsubishi Logo",
     },
     {
-      src: "/images/equipment/brands/trane.png",
+      src: "../../../images/equipment/brands/trane.png",
       alt: "Trane Logo",
     },
     {
-      src: "/images/equipment/brands/york.jpg",
+      src: "../../../images/equipment/brands/york.jpg",
       alt: "York Logo",
     },
   ],
