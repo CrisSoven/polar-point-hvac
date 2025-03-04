ca733ce67a2d87e99a6d3bec23e92c43ae7c17c6
CrisSoven <20213tn044@utez.edu.mx>
2025-03-04 07:11:10 -0600

    [MODIFY] contact form

--- src/components/contactUs/ContactUsForm.jsx
+++ src/components/contactUs/ContactUsForm.jsx
@@ -1,32 +1,28 @@
-import { useState } from "react";
-import { TextField, Button, Grid2, Box, InputAdornment } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Grid,
+  Box,
+  InputAdornment,
+  Typography,
+} from "@mui/material";
 import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import EmailIcon from "@mui/icons-material/Email";
 import PhoneIcon from "@mui/icons-material/Phone";
 import DescriptionIcon from "@mui/icons-material/Description";
-import ReCAPTCHA from "react-google-recaptcha";
 
 const schema = yup.object({
-  nombre: yup.string().required("El nombre es obligatorio"),
-  email: yup
-    .string()
-    .email("El email no es válido")
-    .required("El email es obligatorio"),
+  name: yup.string().required("Name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
   phone: yup
     .string()
-    .matches(/^[0-9]{10}$/, "El teléfono debe ser de 10 dígitos")
-    .required("El teléfono es obligatorio"),
-  serviceAddress: yup
-    .string()
-    .required("La dirección del servicio es obligatoria"),
-  descripcion: yup.string().required("La descripción es obligatoria"),
-  availability: yup.string().required("La disponibilidad es obligatoria"),
-  recaptcha: yup
-    .string()
-    .required("Debes verificar que no eres un robot")
-    .nullable(),
+    .matches(/^[0-9]{10}$/, "Phone must be 10 digits")
+    .required("Phone is required"),
+  serviceAddress: yup.string().required("Service address is required"),
+  description: yup.string().required("Description is required"),
+  availability: yup.array().min(2, "Please select a date range"),
 });
 
 const ContactUsForm = () => {
@@ -34,19 +30,12 @@ const ContactUsForm = () => {
     control,
     handleSubmit,
     formState: { errors },
-    setValue,
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const [captchaVerified, setCaptchaVerified] = useState(false);
 
   const onSubmit = (data) => {
     console.log(data);
-  };
-
-  const handleCaptchaChange = (value) => {
-    setCaptchaVerified(!!value);
-    setValue("recaptcha", value);
   };
 
   return (
@@ -55,33 +44,33 @@ const ContactUsForm = () => {
       onSubmit={handleSubmit(onSubmit)}
       sx={{ maxWidth: 600, margin: "auto" }}
     >
-      <Grid2 container spacing={2}>
-        {/* Nombre */}
-        <Grid2 xs={12}>
+      <Grid container spacing={2}>
+        {/* Name */}
+        <Grid item xs={12}>
+          <Typography>Name</Typography>
           <Controller
-            name="nombre"
+            name="name"
             control={control}
             render={({ field }) => (
               <TextField
                 {...field}
-                label="Nombre"
                 fullWidth
-                error={!!errors.nombre}
-                helperText={errors.nombre?.message}
+                error={!!errors.name}
+                helperText={errors.name?.message}
               />
             )}
           />
-        </Grid2>
+        </Grid>
 
-        {/* Email y Phone */}
-        <Grid2 xs={12} sm={6}>
+        {/* Email */}
+        <Grid item xs={12} sm={6}>
+          <Typography>Email</Typography>
           <Controller
             name="email"
             control={control}
             render={({ field }) => (
               <TextField
                 {...field}
-                label="Email"
                 fullWidth
                 error={!!errors.email}
                 helperText={errors.email?.message}
@@ -95,15 +84,17 @@ const ContactUsForm = () => {
               />
             )}
           />
-        </Grid2>
-        <Grid2 xs={12} sm={6}>
+        </Grid>
+
+        {/* Phone */}
+        <Grid item xs={12} sm={6}>
+          <Typography>Phone</Typography>
           <Controller
             name="phone"
             control={control}
             render={({ field }) => (
               <TextField
                 {...field}
-                label="Phone"
                 fullWidth
                 error={!!errors.phone}
                 helperText={errors.phone?.message}
@@ -117,39 +108,39 @@ const ContactUsForm = () => {
               />
             )}
           />
-        </Grid2>
+        </Grid>
 
         {/* Service Address */}
-        <Grid2 xs={12}>
+        <Grid item xs={12}>
+          <Typography>Service Address</Typography>
           <Controller
             name="serviceAddress"
             control={control}
             render={({ field }) => (
               <TextField
                 {...field}
-                label="Service Address"
                 fullWidth
                 error={!!errors.serviceAddress}
                 helperText={errors.serviceAddress?.message}
               />
             )}
           />
-        </Grid2>
+        </Grid>
 
-        {/* Descripción */}
-        <Grid2 xs={12}>
+        {/* Description */}
+        <Grid item xs={12}>
+          <Typography>Description</Typography>
           <Controller
-            name="descripcion"
+            name="description"
             control={control}
             render={({ field }) => (
               <TextField
                 {...field}
-                label="Descripción"
                 multiline
                 rows={4}
                 fullWidth
-                error={!!errors.descripcion}
-                helperText={errors.descripcion?.message}
+                error={!!errors.description}
+                helperText={errors.description?.message}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
@@ -160,47 +151,15 @@ const ContactUsForm = () => {
               />
             )}
           />
-        </Grid2>
-
-        {/* Disponibilidad */}
-        <Grid2 xs={12}>
-          <Controller
-            name="availability"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Disponibilidad para cita"
-                fullWidth
-                error={!!errors.availability}
-                helperText={errors.availability?.message}
-              />
-            )}
-          />
-        </Grid2>
-
-        {/* <Grid2 xs={12}>
-          <ReCAPTCHA
-            sitekey="YOUR_RECAPTCHA_SITE_KEY"
-            onChange={handleCaptchaChange}
-          />
-
-          {errors.recaptcha && <span>{errors.recaptcha.message}</span>}
-        </Grid2> */}
+        </Grid>
 
         {/* Submit */}
-        <Grid2 xs={12}>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth
-            disabled={!captchaVerified}
-          >
-            Enviar
+        <Grid item xs={12}>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Submit
           </Button>
-        </Grid2>
-      </Grid2>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
